abbbe5241654e6f04cee9c9f13d6d19a3632b360
logiclogue <jordanpaullord@hotmail.co.uk>
2018-08-13 08:02:52 +0100

    Starts from the beginning

--- src/LevelStateStream.js
+++
@@ -1,7 +0,0 @@
-import { LevelState } from "./LevelState";
-import { scan } from "rxjs/operators";
-import { initialGameState } from "./GameStateStream";
-
-export const onChop = initialGameState.pipe(
-    
-);
--- src/MapHelpers.js
+++
@@ -1,9 +0,0 @@
-const set      = key => value => map => map.set(key, value);
-const update   = key => f     => map => map.update(key, f);
-const get      = key          => map => map.get(key);
-const deleteK  = key          => map => map.delete(key);
-const toObject =                 map => map.toObject();
-
-module.exports = {
-    set, update, get, deleteK, toObject
-};
